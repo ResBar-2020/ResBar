cf46268c3acb7738c45245b6d129e625d21f204a
ElmerMartinz <53493325+ElmerMartinz@users.noreply.github.com>
2020-03-27 17:48:05 -0600

    Modulo de ordenes-Dashboard principal-aun no filtra por activos

--- ordenes.js
+++ ordenes.js
@@ -3,6 +3,9 @@ new Vue({
     data: {
         // Aqui inician las propiedades que vamos a necesitar
         //para almacenar nuestros objetos de trabajo
+        textoBusqueda:"",
+        ordenSelected: 0,
+
         ordenes: [{id:"2345ab54c1111", fecha:"2020-10-31", mesero:"Juan",mesa:"2",cliente:"", estado:"A",total:21.10, observacion:"",
 
         detalleOrden:[
@@ -27,38 +30,35 @@ new Vue({
         
         {cantidad:2,nombre:"Refresco de Horchata", precio: 1.75, categoria:{nombre:"Bebidas"},subtotal:1.75}
         
-        ]},
-        {id:"2345ab54c2222", fecha:"2020-10-31", mesero:"Luis",mesa:"5",cliente:"Don Carlos", estado:"A",total:16.00, observacion:"Sin Cebolla",
-        
-        detalleOrden:[
-        
-        {cantidad: 1,nombre:"Hamburguesa Big", precio: 7.25, categoria:{nombre:"Platos"}, subtotal:7.25},
-        
-        {cantidad: 2,nombre:"Ensalada Cesar", precio: 5.55, categoria:{nombre:"Platos"},subtotal:11.10},
-        
-        {cantidad:1,nombre:"Soda Fanta 12 onz", precio: 1.00, categoria:{nombre:"Bebidas"},subtotal:1.00},
-        
-        {cantidad:2,nombre:"Refresco de Horchata", precio: 1.75, categoria:{nombre:"Bebidas"},subtotal:1.75}
-        
-        ]},{id:"2345ab54c2222", fecha:"2020-10-31", mesero:"Luis",mesa:"10",cliente:"Don Carlos", estado:"A",total:16.00, observacion:"Sin Cebolla",
-        
-        detalleOrden:[
-        
-        {cantidad: 1,nombre:"Hamburguesa Big", precio: 7.25, categoria:{nombre:"Platos"}, subtotal:7.25},
-        
-        {cantidad: 2,nombre:"Ensalada Cesar", precio: 5.55, categoria:{nombre:"Platos"},subtotal:11.10},
-        
-        {cantidad:1,nombre:"Soda Fanta 12 onz", precio: 1.00, categoria:{nombre:"Bebidas"},subtotal:1.00},
-        
-        {cantidad:2,nombre:"Refresco de Horchata", precio: 1.75, categoria:{nombre:"Bebidas"},subtotal:1.75}
-        
-        ]}],
+        ]},],
         ascendente: true,
         activos: true,
-        lactivos: null
+        lactivos: null,
+        
     },
+
     methods: {
         //aqui van los metodos que vamos a necesitar
+        buscar:function(x){
+            
+            if(this.textoBusqueda=="")
+                return true;
+            var cad=this.ordenes[x].id+ 
+                this.ordenes[x].cliente+
+                this.ordenes[x].mesa+
+                this.ordenes[x].mesero+
+                this.ordenes[x].observacion+
+                this.ordenes[x].total;        
+            
+            cad=cad.toUpperCase();
+            
+            if(cad.indexOf(this.textoBusqueda.toUpperCase())>=0)
+                        return true;
+            else
+                return false;
+
+        },
+       
         ordenar: function() {
             this.ascendente = !this.ascendente
             this.ordenes.sort( function(a,b){
@@ -69,11 +69,14 @@ new Vue({
                 }
             })
         },
+
         mostrarActivos: function(){
             this.lactivos = this.ordenes.filter(orden => orden.estado == "A")
             console.log(this.lactivos[0].mesa)
             this.activos = !this.activos
-            return this.lactivos        }
+            return this.lactivos    
+    
+        }
         
     }
 
