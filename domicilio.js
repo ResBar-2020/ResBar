3d44d01df3f17e6e48deec83a573d910729ab9ca
Kevin Figueroa <kevinozmin@gmail.com>
2020-05-26 16:54:28 -0600

    agregando pagina para ordenes a domicilio

---
+++ domicilio.js
@@ -0,0 +1,70 @@
+new Vue({
+
+    el: "#appRESBAR",
+    data: {
+        urlApi: `${ApiRestUrl}/ordenes`,
+        ordenes: {},
+        searchDisplay: "",
+        active: true
+    },
+    methods: {
+        /*
+        switch para mostrar todas las ordenes a domicilio
+        */
+        checkDomicilio: function () {
+            this.getAllDomicilios();
+            if (this.active == true) {
+                this.getAllDomicilios();
+            } else {
+                this.getDomicilios();
+            }
+            this.active = !this.active
+
+        },
+        /**
+         * 
+         * @param {od} valor 
+         */
+        domicilioEtapa(orden) {
+            axios.patch(`${this.urlApi}/${valor.id}`, {
+                domicilioEtapa: valor.domicilioEtapa + 1
+            }).then(
+                response => {
+                    if (this.active == true) {
+                        this.getDomicilios();
+                    } else {
+                        this.getAllDomicilios();
+                    }
+                    console.log(response.status);
+                }
+            ).catch(ex => { console.log(ex) })
+
+        },
+        getAllDomicilios: function () {
+            axios.get(this.urlApi + "?filter[where][domicilio]=true").then(
+                response => {
+                    this.ordenes = response.data
+                    console.log(tresponse.data);
+
+                }
+            ).catch(ex => { console.log(ex) })
+        },
+        getDomicilios: function () {
+            axios.get(this.urlApi + "?filter[where][domicilio]=true&filter[where][domicilioEtapa][neq]=2")
+                .then(
+                    response => {
+                        this.ordenes = response.data
+                    }
+                ).catch(ex => { console.log(ex) })
+        },
+        filtro(valor) {
+            if (this.searchDisplay === "") return true;
+            let array = (this.ordenes[valor].id + this.ordenes[valor].cliente).toUpperCase();
+            return array.indexOf(this.searchDisplay.toUpperCase()) >= 0;
+        },
+
+    },
+    mounted() {
+        this.getDomicilios();
+    }
+});
