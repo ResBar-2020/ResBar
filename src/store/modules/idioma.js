1b5d600c561d4a31278f29e9ee12c28dabc387c8
J3Cs <joeybrook235@gmail.com>
2020-11-09 00:51:05 -0600

    el sistema carga el idioma de parametros

--- src/store/modules/idioma.js
+++ src/store/modules/idioma.js
@@ -1,34 +1,44 @@
 import axios from 'axios';
 import credentials from './credentials';
 
-const URL_BASE='http://localhost:5984/idiomas/';
+const URL_BASE = 'http://localhost:5984/idiomas/';
+const URL_PARAMTEROS= 'http://localhost:5984/parametros/';
 const getters = {
     idiomas: state => state.idioma
 };
-const state ={
-idioma :[],
+const state = {
+    idioma: [],
 };
 
 const actions = {
-    async getIdioma({commit}){
+    async getIdioma({commit}) {
+        const parametroIdioma = await axios.post(`${URL_PARAMTEROS}_find`, {
+            "selector": {
+                "_id": "12"
+            }
+        }, credentials.authentication);
+        console.log('idioma: ', parametroIdioma.data.docs[0].valor);
+        
         const response = await axios.post(`${URL_BASE}_find`, {
             "selector": {
-                "_id":"ES"
+                "_id": parametroIdioma.data.docs[0].valor
             }
-            },credentials.authentication);
-        console.log('response data: ',response.data.docs);
-        commit('languajes',response.data.docs);
-    }
+        }, credentials.authentication);
+        console.log('response data: ', response.data.docs[0]);
+        commit('languajes', response.data.docs);
+    },
 };
 
-const mutations={
-    languajes(state,data){
-        console.log(data);
+const mutations = {
+    languajes(state, data) {
         state.idioma = data;
     }
 };
 
 
 export default {
-state, actions, mutations, getters
+    state,
+    actions,
+    mutations,
+    getters
 }
