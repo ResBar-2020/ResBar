a559f7c763de43f4444ff7564482dc9a511be701
J3Cs <joeybrook235@gmail.com>
2020-11-08 23:25:10 -0600

    internacionalizacion y arreglos de disenio

--- src/components/layouts/Layout-login.vue
+++ src/components/layouts/Layout-login.vue
@@ -1,58 +1,62 @@
 <template>
   <v-app>
     <v-main>
-      <v-card width="500" class="mx-auto mt-16" elevation="10">
-        <v-card-title class="primary white--text justify-center text-uppercase">
-          Resbar
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              label="Nombre de usuario"
-              prepend-icon="mdi-account-circle"
-              v-model="user.loggin"
-              :rules="validRules"
-              autocomplete="off"
-            />
-            <v-text-field
-              label="Contraseña"
-              v-model="user.clave"
-              :rules="validRules"
-              :type="showPassword ? 'text' : 'password'"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <div class="text-center">
-          <label v-if="error" class="errorLogin"
-            >El usuario y la contraseña no coinciden</label
+      <div v-for="idioma in idiomas" :key="idioma._id">
+        <v-card width="500" class="mx-auto mt-16" elevation="10">
+          <v-card-title
+            class="primary white--text justify-center text-uppercase"
           >
-        </div>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-row>
-            <v-col class="marg" cols="12">
-              <v-btn
-                class="mx-auto my-3"
-                color="primary"
-                @click="
-                  validate();
-                  authenticate(user);
-                "
-                @keyup.enter="authenticate(user)"
-                >Iniciar Sesión</v-btn
-              >
+            {{ idioma.layouts[0].labels.title }}
+          </v-card-title>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                :label="idioma.layouts[0].labels.username"
+                prepend-icon="mdi-account-circle"
+                v-model="user.loggin"
+                :rules="validRules"
+                autocomplete="off"
+              />
+              <v-text-field
+                :label="idioma.layouts[0].labels.password"
+                v-model="user.clave"
+                :rules="validRules"
+                :type="showPassword ? 'text' : 'password'"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+              />
+            </v-form>
+          </v-card-text>
+          <div class="text-center">
+            <label v-if="error" class="errorLogin"
+              >{{idioma.layouts[0].labels.loginError}}</label
+            >
+          </div>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-row>
+              <v-col class="marg" cols="12">
+                <v-btn
+                  class="mx-auto my-3"
+                  color="primary"
+                  @click="
+                    validate();
+                    authenticate(user);
+                  "
+                  @keyup.enter="authenticate(user)"
+                  >{{ idioma.layouts[0].labels.submit }}</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-card-actions>
+          <v-row justify="center" no-gutters>
+            <v-col class="py-4 text-center marg" cols="12">
+              <span class="copyrigth">{{idioma.layouts[0].labels.copyrigth}}</span>
             </v-col>
           </v-row>
-        </v-card-actions>
-        <v-row justify="center"  no-gutters> 
-        <v-col class="py-4 text-center marg" cols="12">
-          <span class="copyrigth">Copyright © Diseño I 2020</span>
-          </v-col>
-      </v-row>
-      </v-card>
+        </v-card>
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -64,7 +68,7 @@ export default {
     return {
       valid: true,
       showPassword: false,
-      idiomasName:[],
+      idiomasName: [],
       user: {
         loggin: "",
         clave: "",
@@ -73,19 +77,19 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["authenticate","getIdioma"]),
-    
+    ...mapActions(["authenticate", "getIdioma"]),
+
     validate() {
-      this.$refs.form.validate();
+      this.$refs.form[0].validate()
     },
   },
   computed: {
     ...mapState(["username"]),
-    ...mapGetters(["error","idiomas"]),
+    ...mapGetters(["error", "idiomas"]),
   },
   watch: {
     username: function () {
-      this.$router.push({name: "Ordenes"});
+      this.$router.push({ name: "Ordenes" });
       this.$router.go();
     },
   },
@@ -96,13 +100,13 @@ export default {
 };
 </script>
 <style scoped>
-.v-btn:hover{
+.v-btn:hover {
   box-shadow: 0 10px 45px #01579b;
 }
-.copyrigth{
+.copyrigth {
   text-shadow: 0 5px 10px rgba(0, 0, 0, 0.6);
 }
-.marg{
+.marg {
   margin-top: -25px;
 }
 
