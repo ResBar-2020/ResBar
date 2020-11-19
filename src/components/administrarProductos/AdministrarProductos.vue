7f2762dc39d1abae8e79fe824679e1ebade06f4e
Gabriel Ramirez <gxb170@gmail.com>
2020-11-18 18:17:30 -0600

    Se agrega modal nuevo producto

--- src/components/administrarProductos/AdministrarProductos.vue
+++ src/components/administrarProductos/AdministrarProductos.vue
@@ -1,21 +1,28 @@
 <template>
   <v-app>
     <template>
-      <v-container class="grey lighten-5 my-2">
+      <v-container class="lighten-5 my-2">
         <v-row no-gutters>
-          <v-col cols="12" sm="6" md="11">
+          <v-col cols="11" >
             <div mr-3>
               <v-text-field
                 v-model="search"
                 append-icon="mdi-magnify"
-                label="Search"
+                label="Buscar..."
                 single-line
                 hide-details
               ></v-text-field>
             </div>
           </v-col>
-          <v-col cols="6" md="1">
-            <v-btn depressed color="primary"> Agregar </v-btn>
+          <v-col class="mt-2" cols="1" >
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="blue darken-4" dark v-bind="attrs" v-on="on" @click="modalNuevoProducto=true">
+                    Agregar
+                  </v-btn>
+                </template>
+                <span>Nuevo Producto</span>
+              </v-tooltip>
           </v-col>
         </v-row>
       </v-container>
@@ -50,9 +57,16 @@
             <td>No</td>
 
             <td class="mx-2">
-                <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }" >
-                  <v-btn color="blue" dark v-bind="attrs" v-on="on" class="mr-2" small>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="blue"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                    class="mr-2"
+                    small
+                  >
                     <v-icon dark> mdi-pencil </v-icon>
                   </v-btn>
                 </template>
@@ -75,11 +89,17 @@
             <td>Entradas</td>
             <td>No</td>
 
-
             <td>
               <v-tooltip bottom>
                 <template v-slot:activator="{ on, attrs }">
-                  <v-btn color="blue" dark v-bind="attrs" v-on="on" class="mr-2" small>
+                  <v-btn
+                    color="blue"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                    class="mr-2"
+                    small
+                  >
                     <v-icon dark> mdi-pencil </v-icon>
                   </v-btn>
                 </template>
@@ -100,20 +120,85 @@
     </v-simple-table>
 
     <!-- </v-container> -->
+
+    <!-- inicia dialogo modal para agregar nuevo producto -->
+    <template>
+      <v-row justify="center">
+        <v-dialog v-model="modalNuevoProducto" persistent max-width="600px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">Nuevo Producto</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Nombre del producto"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col>
+                    <v-switch
+                      :label="`Es Preparado`"
+                      v-model="esPreparado"
+                    ></v-switch>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-select
+                      :items="['Entrada', 'Plato', 'Bebida', 'Postre']"
+                      label="Categoría"
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Precio $"
+                      type="number"
+                      min="0"
+                      step="0.01"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="red"
+                dark
+                @click="modalNuevoProducto = false"
+              >
+                Cancelar
+              </v-btn>
+              <v-btn
+                color="blue darken-4"
+                dark
+                @click="modalNuevoProducto = false"
+              >
+                Guardar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </template>
   </v-app>
 </template>
 
 <script>
 // import { mapMutations } from "vuex";
 export default {
-  name:"AdministrarProductos",
+  name: "AdministrarProductos",
   data() {
     return {
-      search:""
+      search: "",
+      modalNuevoProducto: false,
+      esPreparado: false,
     };
   },
   methods: {},
 
-  created(){}
+  created() {},
 };
 </script>
