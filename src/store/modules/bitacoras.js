6d338fc2a2a3079f017eb03a5195bc9fbaa62d01
Luis Aguilar <luisggaguilar@gmail.com>
2021-01-13 22:33:33 -0600

    rango de bitacoras (buscar y eliminar)

--- src/store/modules/bitacoras.js
+++ src/store/modules/bitacoras.js
@@ -10,15 +10,65 @@ const getters = {
     allBitacoras: state => state.bitacoras,
 };
 
+async function getAllBitacoras() {
+    const response = await axios.post(`${url}_find`, {
+        "selector": {},
+        "sort": [{ "fecha": "desc" }]
+    }, credentials.authentication);
+    return response;
+}
+
 const actions = {
     //metodo para traer todas las bitacoras
     async getBitacoras({ commit }) {
         const res = await axios.post(`${url}_find`, {
             "selector": {},
-            "sort": [{"fecha" : "desc"}]
+            "sort": [{ "fecha": "desc" }]
         }, credentials.authentication);
         commit('setBitacoras', res.data.docs);
     },
+
+    //metodo para traer rango de bitacoras, (rango es un arreglo que trae fecha inicio y hasta)
+    async getBitacorasRango({ commit }, rango) {
+        const res = await axios.get(`${url}_design/bitacoraRango/_view/bitacoraRango?startkey=
+        "`+ rango[0] + `"&endkey="` + rango[1] + `"`, {
+            "auth": credentials.authentication.auth,
+            "headers": credentials.authentication.headers,
+        }, credentials.authentication);
+        commit('setBitacoras', res.data.rows);
+    },
+
+    //metodo para eliminar rango de bitacoras, (rangoDelete es un arreglo que trae fecha inicio y hasta para eliminar)
+    async deleteBitacorasRango({ commit }, rangoDelete) {
+
+        // se traen el rango de bitacoras a eliminar
+        const res = await axios.get(`${url}_design/bitacoraRango/_view/bitacoraRango?startkey=
+        "`+ rangoDelete[0] + `"&endkey="` + rangoDelete[1] + `"`, {
+            "auth": credentials.authentication.auth,
+            "headers": credentials.authentication.headers,
+        }, credentials.authentication);
+
+        if (res.data.rows.length === 0) {
+            console.log("no hay bitacoras en ese rango")
+        } else {
+            //se itera el arreglo (res)
+            for (let i = 0; i < res.data.rows.length; i++) {
+                //se elimina bitacora del arreglo (res)
+                await axios.delete(`${url}${res.data.rows[i].id}`, {
+                    params: {
+                        "rev": res.data.rows[i].value._rev
+                    },
+                    "auth": credentials.authentication.auth,
+                    "headers": credentials.authentication.headers,
+                }, credentials.authentication);
+
+            }
+        }
+
+        const response = await getAllBitacoras();
+        commit('setBitacoras', response.data.docs);
+    }
+
 };
 
 const mutations = {
--- src/views/Bitacoras.vue
+++ src/views/Bitacoras.vue
@@ -8,7 +8,7 @@
 
     <v-container align-start>
       <v-flex>
-        <v-toolbar flat color="white">
+        <v-toolbar flat>
           <v-row>
             <v-col cols="8" sm="5" md="4">
               <v-text-field
@@ -96,21 +96,42 @@
             </v-col>
             <v-spacer></v-spacer>
 
-            <v-col cols="8" sm="5" md="2">
-              <v-btn color="primary"> BORRAR </v-btn>
+            <v-col cols="8" sm="5" md="2" v-if="botonBuscar">
+              <v-btn class="primary" @click="buscarPorFechas()">
+                BUSCAR POR FECHAS
+              </v-btn>
             </v-col>
             <v-spacer></v-spacer>
+
+            <v-col cols="8" sm="5" md="2" v-if="botonBorrar">
+              <v-btn class="red" color="white" text @click="borrarFiltros()">
+                BORRAR FILTROS
+              </v-btn>
+            </v-col>
           </v-row>
         </v-toolbar>
 
-        <v-data-table
-          :headers="headers"
-          :items="allBitacoras"
-          :items-per-page="5"
-          :search="search"
-          class="mytable"
-        >
-        </v-data-table>
+        <div v-if="sinFiltrarRango">
+          <v-data-table
+            :headers="headers"
+            :items="allBitacoras"
+            :items-per-page="5"
+            :search="search"
+            class="mytable"
+          >
+          </v-data-table>
+        </div>
+
+        <div v-else>
+          <v-data-table
+            :headers="headersFiltrado"
+            :items="allBitacoras"
+            :items-per-page="5"
+            :search="search"
+            class="mytable"
+          >
+          </v-data-table>
+        </div>
 
         <br />
         <div class="text-center">
@@ -131,7 +152,7 @@
                 <br />
                 <br />
 
-                <v-toolbar flat color="white">
+                <v-toolbar flat>
                   <v-row justify="space-around">
                     <v-col md="4">
                       <v-menu
@@ -228,10 +249,26 @@
 
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn class="red" color="white" text @click="dialog = false">
+                <v-btn
+                  class="red"
+                  color="white"
+                  text
+                  @click="
+                    dialog = false;
+                    cancelarEliminar();
+                  "
+                >
                   Cancelar
                 </v-btn>
-                <v-btn class="green" color="white" text @click="dialog = false">
+                <v-btn
+                  class="green"
+                  color="white"
+                  text
+                  @click="
+                    dialog = false;
+                    eliminarBitacoras();
+                  "
+                >
                   Aceptar
                 </v-btn>
               </v-card-actions>
@@ -255,24 +292,32 @@ export default {
   data() {
     return {
       //para el primer Date Picker (DESDE)
-      dateDesde: new Date().toISOString().substr(0, 10),
+      dateDesde: null,
       menuDesde: false,
       //para el segundo Date Picker (HASTA)
-      dateHasta: new Date().toISOString().substr(0, 10),
+      dateHasta: null,
       menuHasta: false,
-
       //para el primer Date Picker (DESDE DELETE)
-      dateDesdeDelete: new Date().toISOString().substr(0, 10),
+      dateDesdeDelete: null,
       menuDesdeDelete: false,
       //para el segundo Date Picker (HASTA DELETE)
-      dateHastaDelete: new Date().toISOString().substr(0, 10),
+      dateHastaDelete: null,
       menuHastaDelete: false,
-
+      //modal eliminar bitacoras
       modal: false,
       dialog: false,
-
+      //buscar de la tabla
       search: "",
+      //juego de botones
+      botonBuscar: true,
+      botonBorrar: false,
+      //para la tabla (true muestra la tabla sin filtrar, false nuestra la filtrada por rango de fechas)
+      sinFiltrarRango: true,
+      //arreglos donde se guardan las fechas (inicio y desde)
+      fechasSelec: [],
+      fechasSelecDelete: [],
 
+      //headers de tabla normal (sin filtrar por fechas)
       headers: [
         {
           text: "Fecha",
@@ -311,6 +356,45 @@ export default {
         },
       ],
 
+      //headers de tabla filtrada por rango de fechas
+      headersFiltrado: [
+        {
+          text: "Fecha",
+          value: "value.fecha",
+          class: "primary white--text",
+          align: "center",
+          sortable: false,
+        },
+        {
+          text: "Acción",
+          value: "value.accion",
+          class: "primary white--text",
+          align: "center",
+          sortable: false,
+        },
+        {
+          text: "Nombre Completo",
+          value: "value.nombreCompleto",
+          class: "primary white--text",
+          align: "center",
+          sortable: false,
+        },
+        {
+          text: "Loggin",
+          value: "value.loggin",
+          class: "primary white--text",
+          align: "center",
+          sortable: false,
+        },
+        {
+          text: "Descripción",
+          value: "value.descripcion",
+          class: "primary white--text",
+          align: "center",
+          sortable: false,
+        },
+      ],
+
       snackbar: {
         message: "desde bitacoras",
         timout: 2000,
@@ -320,8 +404,117 @@ export default {
 
   methods: {
     ...mapMutations(["showMessage"]),
-    ...mapActions(["getBitacoras"]),
+    ...mapActions([
+      "getBitacoras",
+      "getBitacorasRango",
+      "deleteBitacorasRango",
+    ]),
+    /*
+    metodo para mostrar mensajes
+    */
+    showSnackbar(message) {
+      this.snackbar.message = message;
+      this.showMessage(this.snackbar);
+    },
+    /*
+    metodo para borrar los datos del buscador por fechas y buscador por texto 
+    */
+    borrarFiltros() {
+      this.search = "";
+      this.dateDesde = null;
+      this.dateHasta = null;
+      this.botonBuscar = true;
+      this.botonBorrar = false;
+      this.sinFiltrarRango = true;
+      this.getBitacoras();
+      this.showSnackbar("BORRANDO FILTROS DE BUSQUEDA");
+    },
+    /*
+    metodo para buscar por fechas (cada fecha validada)
+    */
+    buscarPorFechas() {
+      if (this.dateDesde === null) {
+        this.showSnackbar("ERROR, Falta la fecha DESDE");
+      }
+      if (this.dateHasta === null) {
+        this.showSnackbar("ERROR, Falta la fecha HASTA");
+      }
+      if (this.dateDesde > this.dateHasta) {
+        this.showSnackbar(
+          "ERROR, La fecha HASTA no puede ser mayor a la fecha DESDE"
+        );
+      }
+      if (this.dateDesde <= this.dateHasta) {
+        if (this.dateDesde === null && this.dateHasta === null) {
+          this.showSnackbar("ERROR, INGRESE LAS FECHAS");
+        } else {
+          this.fechasSelec.push(this.dateDesde);
+          this.fechasSelec.push(this.dateHasta);
+          this.getBitacorasRango(this.fechasSelec);
+          this.sinFiltrarRango = false;
+          this.showSnackbar(
+            "Buscando bitacoras desde la fecha " +
+              this.dateDesde +
+              " hasta la fecha " +
+              this.dateHasta +
+              "."
+          );
+          this.botonBuscar = false;
+          this.botonBorrar = true;
+        }
+      }
+    },
+    /*
+    metodo para eliminar bitacoras por rango de fechas (cada fecha validada)
+    */
+    eliminarBitacoras() {
+      this.search = "";
+      this.dateDesde = null;
+      this.dateHasta = null;
+      this.botonBuscar = true;
+      this.botonBorrar = false;
+
+      if (this.dateDesdeDelete === null) {
+        this.showSnackbar("ERROR AL ELIMINAR, Falta la fecha DESDE");
+      }
+      if (this.dateHastaDelete === null) {
+        this.showSnackbar("ERROR AL ELIMINAR, Falta la fecha HASTA");
+      }
+      if (this.dateDesdeDelete > this.dateHastaDelete) {
+        this.showSnackbar(
+          "ERROR AL ELIMINAR, La fecha HASTA no puede ser mayor a la fecha DESDE"
+        );
+      }
+      if (this.dateDesdeDelete <= this.dateHastaDelete) {
+        if (this.dateDesdeDelete === null && this.dateHastaDelete === null) {
+          this.showSnackbar("ERROR AL ELIMINAR, INGRESE LAS FECHAS");
+        } else {
+          this.fechasSelecDelete.push(this.dateDesdeDelete);
+          this.fechasSelecDelete.push(this.dateHastaDelete);
+          this.deleteBitacorasRango(this.fechasSelecDelete);
+          this.sinFiltrarRango = true;
+          this.getBitacoras();
+          this.showSnackbar(
+            "ELIMINANDO BITACORAS DESDE " +
+              this.dateDesdeDelete +
+              " HASTA " +
+              this.dateHastaDelete +
+              "."
+          );
+        }
+      }
+      this.dateDesdeDelete = null;
+      this.dateHastaDelete = null;
+    },
+    /*
+    metodo para cancelar eliminar por fechas, reinicia las fechas para que no se guarden
+    */
+    cancelarEliminar() {
+      this.dateDesdeDelete = null;
+      this.dateHastaDelete = null;
+    },
   },
+
   created() {
     this.getBitacoras();
   },
@@ -338,10 +531,5 @@ export default {
   padding: 1px;
   font-family: Roboto, sans-serif;
   line-height: 1.5;
-}
-.v-data-table > .v-data-table__wrapper > table > tbody > tr > th,
-.v-data-table > .v-data-table__wrapper > table > tfoot > tr > th,
-.v-data-table > .v-data-table__wrapper > table > header > tr > th {
-    font-size: 100px;
 }
 </style>
